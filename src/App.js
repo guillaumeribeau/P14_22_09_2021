605d92f13266b1fa454aaf4048fbd8ab69c8b991
ribeau <guillaume>
2021-10-09 10:21:02 +0200

    Authentification is ok with private route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import EmployeeList from './Pages/EmployeeList';
-import Home from './Pages/Home';
+import EmployeeList from "./Pages/EmployeeList";
+import Home from "./Pages/Home";
 import Login from "./Pages/Login";
-import {AuthProvider} from "./utils/AuthProvider"
-
+import { AuthProvider } from "./utils/AuthProvider";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <AuthProvider>
-    <Router>  
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/employee" component={EmployeeList} />
-    
-     
-    </Router>
+      <Router>
+        <PrivateRoute exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/employee" component={EmployeeList} />
+      </Router>
     </AuthProvider>
   );
-
-  
-  
 }
 
 export default App;
